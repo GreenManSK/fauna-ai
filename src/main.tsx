de1e828494358ef4b8c8b49401733f7b34d2116e
Lukáš Kurčík <lukas.kurcik@gmail.com>
2025-03-15 16:39:01 +0100

    Create root dir when needed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const prepareElement = (id: string) => {
+  const existingElement = document.getElementById(id);
+  if (existingElement) {
+    return existingElement;
+  }
+  const newElement = document.createElement("div");
+  newElement.id = id;
+  document.body.appendChild(newElement);
+  return newElement;
+};
+
+createRoot(prepareElement("fauna-ai")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
